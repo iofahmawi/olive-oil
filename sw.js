11e998f606de60ab189a64e58876b6a70ee3174d
iofahmawi <mo.fahmawi@gmail.com>
2026-01-22 09:31:54 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'olive-calc-v1';
+const assets = [
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-1024.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
