afb7f667d4144b3fdbc2a8442bcefc3e8f8773b7
iofahmawi <mo.fahmawi@gmail.com>
2026-02-26 10:09:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,25 @@
-const cacheName = 'olive-calc-v49';
-const assets = [
-  './',
-  'index.html',
-  'manifest.json',
-  'icon-1024.png'
+const CACHE_NAME = 'olive-calc-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-1024.png' // تأكد من وجود صورة بهذا الاسم أو غير الاسم في الكود
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+// تثبيت الخدمة وتخزين الملفات
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// استرجاع الملفات من التخزين عند عدم وجود إنترنت
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
-
-
-
