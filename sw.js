2c1c8c4719b868d96a7edae1d0c42c5311a8dc33
iofahmawi <mo.fahmawi@gmail.com>
2026-05-13 12:22:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // رقم النسخة - قم بتغييره (مثلاً من v3 إلى v4) عند إجراء أي تعديل مستقبلي
-const CACHE_NAME = 'olive-calc-v16'; 
+const CACHE_NAME = 'olive-calc-v17'; 
 const ASSETS = [
   './',
   './index.html',
